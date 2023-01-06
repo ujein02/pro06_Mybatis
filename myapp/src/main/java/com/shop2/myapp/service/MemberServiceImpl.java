4a06007c94ffc2228f98812656531f8adf5d892d
ujein02 <ujein02@gmail.com>
2023-01-06 18:25:34 +0900

    0106 update member

---
+++ myapp/src/main/java/com/shop2/myapp/service/MemberServiceImpl.java
@@ -0,0 +1,40 @@
+package com.shop2.myapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shop2.myapp.dto.MemberDTO;
+import com.shop2.myapp.model.MemberMapper;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+	
+	@Autowired
+	MemberMapper memberMapper;
+
+	@Override
+	public List<MemberDTO> memberList() throws Exception {
+		return memberMapper.memberList();
+	}
+
+	@Override
+	public MemberDTO getUser(String userId) throws Exception {
+		return memberMapper.getUser(userId);
+	}
+
+	@Override
+	public MemberDTO getLogin(String userId, String userPw) throws Exception {
+		return memberMapper.getLogin(userId, userPw);
+	}
+
+	@Override
+	public void signUser(MemberDTO member) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
